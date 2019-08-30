c75d8d016d5874cf1137c6ee08c3745ccd729783
Ali Kucukavci <ali91kavci@outlook.dk>
2019-08-30 13:05:39 +0200

    third commit

--- data/buildingInfo.js
+++ data/buildingInfo.js
@@ -54,63 +54,77 @@ class Room {
 }
 
 //instantiate a room
-const room1 = new Room(1, 1, 'B101', 'Foyer', 7.9, 2.6, 20.5, 1, 2, 3, 4, 5, 0, 1, 1);
+//const room19 = new Room(2, 2, 'B202', 'Test 1', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 30, 1, 1);
+createRoom(home.rooms, 1, 1, 'B101', 'Foyer', 7.9, 2.6, 20.5, 1, 2, 3, 4, 5, 0, 1, 1)
 
-const room2 = new Room(1, 1, 'B102', 'Living Room', 30, 2.6, 78, 0, 0, 0, 0, 2, 0, 1, 1);
+createRoom(home.rooms, 1, 1, 'B102', 'Living Room', 30, 2.6, 78, 0, 0, 0, 0, 2, 0, 1, 1)
 
-const room3 = new Room(1, 1, 'B103', 'Kitchen', 14, 2.6, 36.4, 0, 0, 0, 0, 3, 0, 1, 1);
+createRoom(home.rooms, 1, 1, 'B103', 'Kitchen', 14, 2.6, 36.4, 0, 0, 0, 0, 3, 0, 1, 1)
 
-const room4 = new Room(1, 1, 'B104', 'WC', 3, 2.6, 7.8, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 1, 1, 'B104', 'WC', 3, 2.6, 7.8, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room5 = new Room(2, 1, 'B203', 'Bedroom 2', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 1, 'B203', 'Bedroom 2', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 0, 1, 1)
 
-const room6 = new Room(2, 1, 'B201', 'Hallway', 7.8, 2.6, 20.3, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 1, 'B201', 'Hallway', 7.8, 2.6, 20.3, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room7 = new Room(2, 1, 'B204', 'Bathroom 2', 5, 2.6, 13, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 1, 'B204', 'Bathroom 2', 5, 2.6, 13, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room8 = new Room(2, 1, 'B205', 'Utility', 2, 2.6, 5.2, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 1, 'B205', 'Utility', 2, 2.6, 5.2, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room9 = new Room(2, 1, 'B202', 'Bedroom 1', 26, 2.6, 67.6, 10, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 1, 'B202', 'Bedroom 1', 26, 2.6, 67.6, 10, 0, 0, 0, 0, 0, 1, 1)
 
-const room10 = new Room(1, 2, 'B101', 'Foyer', 17.9, 2.6, 20.5, 0, 0, 0, 5, 0, 0, 1, 1);
+createRoom(home.rooms, 1, 2, 'B101', 'Foyer', 17.9, 2.6, 20.5, 0, 0, 0, 5, 0, 0, 1, 1)
 
-const room11 = new Room(1, 2, 'B102', 'Living Room', 30, 2.6, 78, 4, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 1, 2, 'B102', 'Living Room', 30, 2.6, 78, 4, 0, 0, 0, 0, 0, 1, 1)
 
-const room12 = new Room(1, 2, 'B103', 'Kitchen', 14, 2.6, 36.4, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 1, 2, 'B103', 'Kitchen', 14, 2.6, 36.4, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room13 = new Room(1, 2, 'B104', 'WC', 3, 2.6, 7.8, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 1, 2, 'B104', 'WC', 3, 2.6, 7.8, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room14 = new Room(2, 2, 'B203', 'Bedroom 2', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 2, 'B203', 'Bedroom 2', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 0, 1, 1)
 
-const room15 = new Room(2, 2, 'B201', 'Hallway', 7.8, 2.6, 20.3, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 2, 'B201', 'Hallway', 7.8, 2.6, 20.3, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room16 = new Room(2, 2, 'B204', 'Bathroom 2', 5, 2.6, 13, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 2, 'B204', 'Bathroom 2', 5, 2.6, 13, 0, 0, 0, 0, 0, 0, 1, 1 )
 
-const room17 = new Room(2, 2, 'B205', 'Utility', 2, 2.6, 5.2, 0, 0, 0, 0, 0, 0, 1, 1);
+createRoom(home.rooms, 2, 2, 'B205', 'Utility', 2, 2.6, 5.2, 0, 0, 0, 0, 0, 0, 1, 1)
 
-const room18 = new Room(2, 2, 'B202', 'Bedroom 1', 26, 2.6, 67.6, 2, 0, 0, 0, 0, 30, 1, 1);
+createRoom(home.rooms, 2, 2, 'B204', 'Bedroom 1', 26, 2.6, 67.6, 10, 0, 0, 0, 0, 0, 1, 1)
 
-home.rooms.push(
-    room1,
-    room2,
-    room3,
-    room4,
-    room5,
-    room6,
-    room7,
-    room8,
-    room9,
-    room10,
-    room11,
-    room12,
-    room13,
-    room14,
-    room15,
-    room16,
-    room17,
-    room18
-);
+createRoom(home.rooms, 2, 2, 'B204', 'Bedroom 1', 26, 2.6, 67.6, 10, 0, 0, 0, 0, 0, 1, 1)
 
+
+function createRoom(arr,level,
+    apartment,
+    number,
+    name,
+    area,
+    height,
+    volume,
+    occupants,
+    airExchange,
+    occupantAirflow,
+    emissionAirflow,
+    specificAirflow,
+    maxAirflow,
+    VEIN,
+    VEOUT) {
+    return arr.push(new Room(level,
+        apartment,
+        number,
+        name,
+        area,
+        height,
+        volume,
+        occupants,
+        airExchange,
+        occupantAirflow,
+        emissionAirflow,
+        specificAirflow,
+        maxAirflow,
+        VEIN,
+        VEOUT))
+}
 let buildings = [];
 buildings.push(home);
 console.log(home.rooms);
--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
 //First I compare building type, after match I loop over current rooms. For every current room I loop over previous (experience) rooms. When match is correct I add the specific airflow.
 // Accessing building types of Ebuilding
 
-let myAirQuality
-for (let i = 0; i < comfort.airQuality.length; i++){
-    if (home.airQuality === comfort.airQuality[i].name){
+let myAirQuality;
+for (let i = 0; i < comfort.airQuality.length; i++) {
+    if (home.airQuality === comfort.airQuality[i].name) {
         myAirQuality = comfort.airQuality[i].value;
-    }  
+    }
 }
-console.log(myAirQuality)
+console.log(myAirQuality);
 
-let myPollution
-for (let i = 0; i < comfort.pollution.length; i++){
-    if (home.pollution === comfort.pollution[i].name){
+let myPollution;
+for (let i = 0; i < comfort.pollution.length; i++) {
+    if (home.pollution === comfort.pollution[i].name) {
         myPollution = comfort.pollution[i].value;
-    }  
+    }
 }
-console.log(room1.occupants)
-
-
+// console.log(room1.occupants)
 
 for (let i = 0; i < eBuildings.types.length; i++) {
     if (home.type === eBuildings.types[i].type) {
@@ -28,24 +26,27 @@ for (let i = 0; i < eBuildings.types.length; i++) {
                 if (home.rooms[k].name === eBuildings.types[i].Rooms[j].name) {
                     home.rooms[k].specificAirflow = eBuildings.types[i].Rooms[j].specificAirflow;
                 } else {
-                    home.rooms[k].occupantAirflow = (home.rooms[k].area *myPollution +home.rooms[k].occupants*myAirQuality).toFixed(0)
-                    console.log('nothing again');
+                    home.rooms[k].occupantAirflow = (
+                        home.rooms[k].area * myPollution +
+                        home.rooms[k].occupants * myAirQuality
+                    ).toFixed(0);
+                    // console.log('nothing again');
                 }
 
                 if (home.rooms[k].specificAirflow !== 0) home.rooms[k].occupantAirflow = 0;
             }
         }
     } else {
-
         // home.rooms[k].occupantAirflow = (home.rooms[k].area *myPollution +home.rooms[k].occupants*myAirQuality).toFixed(0)
         //not matching home types
-        console.log('nothing');
+        // console.log('nothing');
     }
 }
 
 if (home.rooms.length > 0) {
-    document.body.querySelector('.section').innerHTML +=` 
-    <table class="table">
+    document.body.querySelector('.section').innerHTML += ` 
+    <table class="table table is-hoverable is-fullwidth
+    ">
         <thead>
           <tr class="tableHead">
                    
@@ -56,66 +57,60 @@ if (home.rooms.length > 0) {
             <tr></tr>
         </tfoot>
         </tbody>
-      </table>`
+      </table>`;
 }
 
 const headings = {
-    area: "area (m2)",
-    height: "height (m)",
-    occupants: "occupants (pcs)",
-    airExchange: "airExchange (h-1)",
-    occupantAirflow: "occupantAirflow (l/s)",
-    emissionAirflow: "emissionAirflow (l/s)",
-    specificAirflow: "specificAirflow (l/s)",
-    maxAirflow: "maxAirflow (l/s)"
+    area: 'area (m2)',
+    height: 'height (m)',
+    occupants: 'occupants (pcs)',
+    airExchange: 'airExchange (h-1)',
+    occupantAirflow: 'occupantAirflow (l/s)',
+    emissionAirflow: 'emissionAirflow (l/s)',
+    specificAirflow: 'specificAirflow (l/s)',
+    maxAirflow: 'maxAirflow (l/s)'
+};
 
-
-}
-
-for (let i = 0; i < Object.keys(room1).length; i++) {
-    const heading = headings[Object.keys(room1)[i]] || Object.keys(room1)[i]
+for (let i = 0; i < Object.keys(home.rooms[0]).length; i++) {
+    const heading = headings[Object.keys(home.rooms[0])[i]] || Object.keys(home.rooms[0])[i];
     document.body.querySelector('.tableHead').innerHTML += `<th><abbr>${heading}</abbr></th>`;
 }
-console.log(Object.keys(room1))
+// console.log(Object.keys(room1))
 // calculate sum maxAirflow for each room
 
 let rowMaxAirflow = 0;
 for (let i = 0; i < home.rooms.length; i++) {
     rowMaxAirflow = Math.max(
-            +home.rooms[i].airExchange,
-            +home.rooms[i].occupantAirflow,
-            +home.rooms[i].emissionAirflow,
-            +home.rooms[i].specificAirflow
+        +home.rooms[i].airExchange,
+        +home.rooms[i].occupantAirflow,
+        +home.rooms[i].emissionAirflow,
+        +home.rooms[i].specificAirflow
     );
-    console.log(rowMaxAirflow)
-    home.rooms[i].maxAirflow = rowMaxAirflow 
-    }
-    
-
-
-
+    // console.log(rowMaxAirflow)
+    home.rooms[i].maxAirflow = rowMaxAirflow;
+}
 
 for (let i = 0; i < home.rooms.length; i++) {
-    document.body.querySelector('.roomRow').innerHTML += `<th><abbr>${home.rooms[i].level}</abbr></th>
-    <th><abbr>${home.rooms[i].apartment}</abbr></th>
-    <th><abbr>${home.rooms[i].number}</abbr></th>
-    <th><abbr>${home.rooms[i].name}</abbr></th>
-    <th><abbr>${home.rooms[i].area}</abbr></th>
-    <th><abbr>${home.rooms[i].height}</abbr></th>
-    <th><abbr>${home.rooms[i].volume}</abbr></th>
-    <th><abbr>${home.rooms[i].occupants}</abbr></th>
-    <th><abbr>${home.rooms[i].airExchange}</abbr></th>
-    <th><abbr>${home.rooms[i].occupantAirflow}</abbr></th>
-    <th><abbr>${home.rooms[i].emissionAirflow}</abbr></th>
-    <th><abbr>${home.rooms[i].specificAirflow}</abbr></th>
-    <th><abbr>${home.rooms[i].maxAirflow}</abbr></th>
-    <th><abbr>${home.rooms[i].VEIN}</abbr></th>
-    <th><abbr>${home.rooms[i].VEOUT}</abbr></th>
+    document.body.querySelector('.roomRow').innerHTML += `<td><abbr>${home.rooms[i].level}</abbr></td>
+    <td><abbr>${home.rooms[i].apartment}</abbr></td>
+    <td><abbr>${home.rooms[i].number}</abbr></td>
+    <td><abbr>${home.rooms[i].name}</abbr></td>
+    <td><abbr>${home.rooms[i].area}</abbr></td>
+    <td><abbr>${home.rooms[i].height}</abbr></td>
+    <td><abbr>${home.rooms[i].volume}</abbr></td>
+    <td><abbr>${home.rooms[i].occupants}</abbr></td>
+    <td><abbr>${home.rooms[i].airExchange}</abbr></td>
+    <td><abbr>${home.rooms[i].occupantAirflow}</abbr></td>
+    <td><abbr>${home.rooms[i].emissionAirflow}</abbr></td>
+    <td><abbr>${home.rooms[i].specificAirflow}</abbr></td>
+    <td><abbr>${home.rooms[i].maxAirflow}</abbr></td>
+    <td><abbr>${home.rooms[i].VEIN}</abbr></td>
+    <td><abbr>${home.rooms[i].VEOUT}</abbr></td>
     `;
 }
 
-console.log(document.body.querySelector('.roomSum').innerHTML);
-for (let i = 0; i < Object.keys(room1).length; i++) {
+// console.log(document.body.querySelector('.roomSum').innerHTML);
+for (let i = 0; i < Object.keys(home.rooms[0]).length; i++) {
     document.body.querySelector('.roomSum tr').innerHTML += `<th><abbr>${''}</abbr></th>`;
 }
 
@@ -179,81 +174,141 @@ for (let i = 0; i < home.rooms.length; i++) {
     document.body.querySelector('.roomSum tr :nth-child(13)').innerHTML = `<th><abbr>${sumMaxAirflow}</abbr></th>`;
 }
 
+
+//Sorting numbers before creating graph
+let homeRoomsSortedArea = home.rooms.slice().sort((a,b)=>b.area - a.area)
+console.log(homeRoomsSortedArea)
+
+let homeRoomsSortedmaxAirflow = home.rooms.slice().sort((a,b)=>b.maxAirflow - a.maxAirflow)
+console.log(homeRoomsSortedmaxAirflow)
+
+
 let newArrArea = [];
 for (let i = 0; i < home.rooms.length; i++) {
-    newArrArea.push(home.rooms[i].area)
+    newArrArea.push(homeRoomsSortedArea[i].area);
 }
 
 let maxAirFlowData = [];
 for (let i = 0; i < home.rooms.length; i++) {
-    maxAirFlowData.push(home.rooms[i].maxAirflow)
+    maxAirFlowData.push(homeRoomsSortedmaxAirflow[i].maxAirflow);
 }
 
-
-let newArrName = [];
+let newArrNameArea = [];
 for (let i = 0; i < home.rooms.length; i++) {
-    newArrName.push(home.rooms[i].name)
+    newArrNameArea.push(homeRoomsSortedArea[i].name);
 }
 
+let newArrNamemaxAirflow = [];
+for (let i = 0; i < home.rooms.length; i++) {
+    newArrNamemaxAirflow.push(homeRoomsSortedmaxAirflow[i].name);
+}
+
+
+
+// Giving each room dataset a color
+function getRandomColor(n) {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    var colors = [];
+    for (var j = 0; j < n; j++) {
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        colors.push(color);
+        color = '#';
+    }
+    return colors;
+}
+
+let roomColors = getRandomColor(home.rooms.length);
+
+//Creating my chart empty
 var ctx = document.getElementById('myChart').getContext('2d');
+Chart.defaults.global.defaultFontSize = 16;
+Chart.defaults.global.defaultFontColor = "#777";
 
-var myChart = new Chart(ctx,undefined)
 
-drawChart(undefined)
+var myChart = new Chart(ctx, undefined);
 
+
+drawChart(undefined);
+
+//Onchange give me the graph
 function changeChart(string) {
     switch (string) {
-        case "maxAirFlowData":
-            drawChart(maxAirFlowData)
+        case 'maxAirFlowData':
+            drawChart(newArrNamemaxAirflow, maxAirFlowData);
             break;
-        case "newArrArea":
-            drawChart(newArrArea)
+        case 'newArrArea':
+            drawChart(newArrNameArea, newArrArea);
             break;
         default:
-            drawChart(undefined)
+            drawChart(undefined);
     }
 }
 
+//Function that creates my graph
+function drawChart(label, data) {
+    // console.log(data)
+    
+    myChart.destroy();
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: label,
+            datasets: [
+                {
+                    label: "Hello",
+                    data: data,
+                    backgroundColor: roomColors,
+                    borderColor: "#777",
+                    borderWidth: 1,
+                    hoverBorderWidth: 3,
+                    hoverBorderColor: "#000",
+                }]
+        },
+        options: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: "Airflow and area distribution",
+                fontSize: 20,
+            },
+            tooltips: {
+               enabled:true 
+            },
 
+            scales: {
+                yAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Value",
+            
+                        },
+                        ticks: {
+                            beginAtZero: true,
 
+                        }
+                    }
+                ],
+                xAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Name",
+            
+                        },
+                        ticks: {
+                            beginAtZero: true,
 
-function drawChart(data) {
-    console.log(data)
-    myChart.destroy()
-     myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: newArrName,
-        datasets: [{
-            label: 'maxAirflow (l/s)',
-            data: data,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+                        }
+                    }
+                ]
+            }
         }
-    }
-});
+    });
 }
+
